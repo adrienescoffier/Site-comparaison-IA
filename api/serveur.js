5c8b84eb3e66122c8835c839c26b65c906394deb
adrienescoffier <adrienescoffierlebg@gmail.com>
2026-09-21 13:35:59 +0000

    création du tableau des ia et app.get

--- api/serveur.js
+++ api/serveur.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const app = express();
 
+let ias = [
+  { id: 1, nom: "Chat GPT", prix: 25 },
+  { id: 2, nom: "Claude", prix: 15 },
+  { id: 3, nom: "Gemini", prix: 120 }
+];
+// GET /produits -> renvoie tout le tableau
+app.get("/ias", (req, res) => {
+  res.json(ias);
+});
 
 // Route de test : GET /
 app.get("/", (req, res) => {
